16b5c4175400fc4101842648173082fbc4989468
end-4 <97237370+end-4@users.noreply.github.com>
2026-05-12 21:05:13 +0200

    not use deprecated ReadyForScene event

--- UserHints.cs
+++ UserHints.cs
@@ -1,21 +1,26 @@
 ﻿using System.IO;
 using Notiffy.API;
+using UnityEngine.SceneManagement;
 
 namespace Notiffy;
 
 public static class UserHints {
-    public static void IssueFirstRunNoticeIfNecessary() {
-        if (ConfigManager.FirstRun.value) {
-            ConfigManager.FirstRun.value = false;
-            NotificationSystem.NotifySend("<color=#f2ca3a>Notiffy</color>: instructions",
-                $"To open notification panel, press [{ConfigManager.GetFormattedPanelKeybind()}]. This can be configured in <color=#55c7f6>Options > Plugin Config > Notiffy</color>",
-                urgency: Urgency.Critical, iconFilePath: Path.Combine(NotiffyPlugin.workingDir, "icon.png"));
-        }
+    private static void IssueFirstRunNoticeIfNecessary() {
+        if (!ConfigManager.FirstRun.value) return;
+        ConfigManager.FirstRun.value = false;
+        NotificationSystem.NotifySend("<color=#f2ca3a>Notiffy</color>: instructions",
+            $"To open notification panel, press [{ConfigManager.GetFormattedPanelKeybind()}]. This can be configured in <color=#55c7f6>Options > Plugin Config > Notiffy</color>",
+            urgency: Urgency.Critical, iconFilePath: Path.Combine(NotiffyPlugin.workingDir, "icon.png"));
     }
 
-    public static void Initialize() {}
+    private static void OnSceneLoaded(Scene s, LoadSceneMode m) {
+        IssueFirstRunNoticeIfNecessary();
+    }
+
+    public static void Initialize() {
+    }
 
     static UserHints() {
-        NotificationSystem.ReadyForScene += IssueFirstRunNoticeIfNecessary;
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 }
